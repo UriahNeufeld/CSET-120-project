1c380532d298d1205c23ad6dc1eb8e294a55575c
Uriah Neufeld <117602071+UriahNeufeld@users.noreply.github.com>
2022-12-12 13:02:52 -0500

    Add files via upload

---
+++ Final Project/mana.js
@@ -0,0 +1,127 @@
+function add() {
+    let newItem=prompt("Add Item Here!")
+    var list = document.getElementById('list'),
+        item = document.createElement('p');
+    item.innerHTML = '<input type="checkbox" />' + newItem;
+    list.appendChild(item);
+}
+
+function rem() {
+    var list = document.getElementById('list'),
+        items = Array.prototype.slice.call(list.childNodes),
+        item;
+    while (item = items.pop()) {
+        if (item.firstChild && item.firstChild.checked) {
+            list.removeChild(item);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function add1() {
+    let newItem=prompt("Add Item Here!")
+    var list = document.getElementById('list1'),
+        item = document.createElement('p');
+    item.innerHTML = '<input type="checkbox" />' + newItem;
+    list.appendChild(item);
+}
+
+function rem1() {
+   
+    var list = document.getElementById('list1'),
+        items = Array.prototype.slice.call(list.childNodes),
+        item;
+    while (item = items.pop()) {
+        if (item.firstChild && item.firstChild.checked) {
+            list.removeChild(item);
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function add2() {
+    let newItem=prompt("Add Item Here!")
+    var list = document.getElementById('list2'),
+        item = document.createElement('p');
+    item.innerHTML = '<input type="checkbox" />' + newItem;
+    list.appendChild(item);
+}
+
+function rem2() {
+ 
+    var list = document.getElementById('list2'),
+        items = Array.prototype.slice.call(list.childNodes),
+        item;
+    while (item = items.pop()) {
+        if (item.firstChild && item.firstChild.checked) {
+            list.removeChild(item);
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function add3() {
+    let newItem=prompt("Add Item Here!")
+    var list = document.getElementById('list3'),
+        item = document.createElement('p');
+    item.innerHTML = '<input type="checkbox" />' + newItem;
+    list.appendChild(item);
+}
+
+function rem3() {
+   
+    var list = document.getElementById('list3'),
+        items = Array.prototype.slice.call(list.childNodes),
+        item;
+    while (item = items.pop()) {
+        if (item.firstChild && item.firstChild.checked) {
+            list.removeChild(item);
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function add4() {
+    let newItem=prompt("Add Item Here!")
+    var list = document.getElementById('list4'),
+        item = document.createElement('p');
+    item.innerHTML = '<input type="checkbox" />' + newItem;
+    list.appendChild(item);
+}
+
+function rem4() {
+    var list = document.getElementById('list4'),
+        items = Array.prototype.slice.call(list.childNodes),
+        item;
+    while (item = items.pop()) {
+        if (item.firstChild && item.firstChild.checked) {
+            list.removeChild(item);
+        }
+    }
+}
+
+function add5() {
+    let newItem=prompt("Add Item Here!")
+    var list = document.getElementById('list5'),
+        item = document.createElement('p');
+    item.innerHTML = '<input type="checkbox" />' + newItem;
+    list.appendChild(item);
+}
+
+function rem5() {
+    var list = document.getElementById('list5'),
+        items = Array.prototype.slice.call(list.childNodes),
+        item;
+    while (item = items.pop()) {
+        if (item.firstChild && item.firstChild.checked) {
+            list.removeChild(item);
+        }
+    }
+}
+
+
+
